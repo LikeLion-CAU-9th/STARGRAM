b13e8aa561ebcfbb4be3586bca45a3076085b7bf
k-mover <rlaxodud3079@gmail.com>
2021-11-08 18:10:00 +0900

    create : kakao url function

---
+++ js/kakaoShare.js
@@ -0,0 +1,24 @@
+
+Kakao.Link.createDefaultButton({
+    container: '#create-kakao-link-btn',
+    objectType: 'feed',
+    content: {
+        title: '나만의 추억의 별자리는?',
+        description: '여행 사진으로 만드는 나만의 추억 별자리',
+        imageUrl:
+        '../image/url-thumnail.jpeg',
+        link: {
+            mobileWebUrl: 'http://stargram.site',
+            webUrl: 'http://stargram.site',
+        },
+        },
+    buttons: [{
+            title: '별자리 만들러 가기',
+            link: {
+            mobileWebUrl: 'http://stargram.site',
+            webUrl: 'http://stargram.site',
+            },
+        },
+    ],
+    
+    })
