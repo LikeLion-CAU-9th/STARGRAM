788565593c501c9da0f799e3df02473159ab7c97
youngkwon02 <youngkwon02@naver.com>
2021-11-17 00:40:58 +0900

    feat: pinned page viewport and style

--- js/pinned.js
+++ js/pinned.js
@@ -24,14 +24,13 @@ const pinned_main = () => {
 
   // Kakao map arrangement
   let mapCenter = center();
-  console.log(mapCenter);
   var mapContainer = document.getElementById("map"), // 지도를 표시할 div
     mapOption = {
       center: new kakao.maps.LatLng(
         mapCenter["latitude"],
         mapCenter["longitude"]
       ), // 지도의 중심좌표
-      level: 4, // 지도의 확대 레벨
+      level: 9, // 지도의 확대 레벨
     };
 
   var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
